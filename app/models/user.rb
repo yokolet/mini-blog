87c176fb0d2121aea61e4de1c48df38e6c729073
Yoko Harada <yokolet@gmail.com>
2023-03-18 16:34:46 +0900

    updates user model to have password_digest and authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  attr_accessor :token
+
+  has_secure_password
+
   has_many :posts, dependent: :destroy
 
   validates :email, uniqueness: true
+  validates :password, length: { minimum: 8 }, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_085421) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_144742) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", limit: 50, null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_085421) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
